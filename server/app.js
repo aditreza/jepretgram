bd37b05bfb004139675d406edc9612e2b1951460
aditReza <lete.note3@gmail.com>
2017-12-07 13:34:01 +0700

    routes

--- server/app.js
+++ server/app.js
@@ -6,8 +6,30 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 const app = express()
 
+//parse application/json and look for raw text      
+app.use(morgan('dev'))         
+app.use(cors())                         
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));
+
+// connect db
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/aditreza')
+.then(function(){
+  console.log('[+] database is listen');
+}).catch(function(err){
+  if(err){
+    console.error(err)
+  }
+})
+
 //routes
-app.get("/", (req, res) => res.json({message: "Welcome to hacktiveoverflow API!"}))
+const users = require('./routes/user')
+
+app.get('/', (req, res) => res.json({message: "Welcome to jepretgram API!"}))
+app.use('/api/users', users)
 
 app.listen(PORT, function(err){
   if(!err){
